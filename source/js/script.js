50c3ff2f461f9fa4405d40e6e20182392a42858d
k0missar <k0missar@yandex.ru>
2023-02-10 23:02:21 +0300

    index.html адаптирован для mobile, tablet, desktop. Готов js slider для прайс таблицы.

--- source/js/script.js
+++ source/js/script.js
@@ -5,7 +5,6 @@ const openMenu = header.querySelector('.header__open-menu');
 const openMenuBtn = header.querySelector('.header__open-menu-svg');
 const closeMenuBtn = header.querySelector('.header__close-menu-svg');
 
-console.log(openMenuBtn.classList.contains('header__button-menu--disabled'));
 if (openMenuBtn.classList.contains('header__button-menu--disabled') == false && closeMenuBtn.classList.contains('header__button-menu--disabled') == false) {
   openMenu.addEventListener('click', () => {
     mainMenu.classList.toggle('main-menu--open');
@@ -14,3 +13,25 @@ if (openMenuBtn.classList.contains('header__button-menu--disabled') == false && 
     closeMenuBtn.classList.toggle('header__menu-btn--open');
   })
 }
+
+const controlItem = document.querySelectorAll('.price__controls-item');
+const priceList = document.querySelector('.price__list');
+let controlList = [];
+let positionLeft = -254;
+
+for (let i = 0; i < controlItem.length; i++) {
+  controlList.push(positionLeft + 'px');
+  positionLeft -= 276;
+}
+
+controlItem.forEach((element, index) => {
+  element.addEventListener('click', ()=> {
+    controlItem.forEach(itemClass => {
+      itemClass.classList.remove('controls__item--current');
+    });
+    element.classList.add('controls__item--current');
+    priceList.style.left = controlList[index];
+  });
+});
+
+
